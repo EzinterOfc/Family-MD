fac3fcb02888d225b55b97798045095417e6f940
Rafi Lukmanu Faizal <104605530+EzinterOfc@users.noreply.github.com>
2022-07-26 15:03:48 +0700

    donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,10 +1,10 @@
 let handler = async (m, { conn, usedPrefix }) => conn.sendButton(m.chat, `
 ╭─「 Donasi • Dana 」
-│ • Telkomsel [081320170984]
-│ • Dana  [081320170984]
+│ • Telkomsel [082153954737]
+│ • Dana  [085600380823]
 ╰────
 ╭─「 *NOTE* 」
-│ > Ingin donasi? Wa.me/6281320170984
+│ > Ingin donasi? Wa.me/6285600380823
 │ _Hasil donasi akan digunakan buat sewa_
 │ _atau beli *RDP/VPS* agar bot bisa jalan_
 │ _24jam tanpa kendala_
